7f7fc30c55797c0a1459063654526c1b8fa0dc33
superdem <superdem@kompeder>
2016-10-11 00:53:48 +0300

    karma unit test ortami hem manuel calistirma icin hemde continous integration icin hazirlandi.

---
+++ src/main/java/erdemc/SpringAngularDeneme1Application.java
@@ -0,0 +1,12 @@
+package erdemc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularDeneme1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAngularDeneme1Application.class, args);
+	}
+}
